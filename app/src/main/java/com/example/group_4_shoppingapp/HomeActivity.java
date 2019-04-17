fc8864d024582a68d59ab92e4b33b45f2db48216
swan <chandanswan@gmail.com>
2019-04-16 21:36:24 -0400

    Fragments Added

--- app/src/main/java/com/example/group_4_shoppingapp/HomeActivity.java
+++ app/src/main/java/com/example/group_4_shoppingapp/HomeActivity.java
@@ -1,17 +1,20 @@
 package com.example.group_4_shoppingapp;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -22,6 +25,10 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open,R.string.close);
 
         mDrawerLayout.addDrawerListener(mToggle);
@@ -29,6 +36,42 @@ public class HomeActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //if(savedInstanceState == null) {
+          //  getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+            //        new ProductsFragment()).commit();
+            //navigationView.setCheckedItem(R.id.nav_product);
+        //}
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_product:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProductsFragment()).commit();
+                break;
+            case R.id.nav_cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CartFragment()).commit();
+                break;
+            case R.id.nav_aboutUs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutFragment()).commit();
+                break;
+            case R.id.nav_contactUs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactFragment()).commit();
+                break;
+            case R.id.nav_logout:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactFragment()).commit();
+                break;
+
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/example/group_4_shoppingapp/LogoutFragment.java
@@ -0,0 +1,30 @@
+package com.example.group_4_shoppingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class LogoutFragment extends AppCompatActivity {
+
+    //@Nullable
+    //@Override
+    //public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.activity_login_main, container, false);
+    //}
+//}
+
+
+
+    <intent> startActivity(intent) {
+        Intent intent = new Intent(LogoutFragment.this,LoginMainActivity.class);
+
+    }
+}
+//}
